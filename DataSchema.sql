00e668f6a05c40445d23cd9a4b3db26e5aa492d6
Лісєєв Кирило Михайлович <127152277+LiseevKirill@users.noreply.github.com>
2025-01-11 11:24:22 +0200

    Update DataSchema.sql

--- DataSchema.sql
+++ DataSchema.sql
@@ -1,18 +1,18 @@
 -- Створення таблиці "Користувач"
 CREATE TABLE Користувач (
-    Ідентифікатор VARCHAR(36) PRIMARY KEY, -- UUID
-    Ім'я VARCHAR(50) NOT NULL CHECK (Ім'я ~ '^[A-ZА-ЯІЇЄҐ][a-zа-яіїєґ]+$'), -- Перше слово з великої літери
-    Прізвище VARCHAR(50) NOT NULL CHECK (Прізвище ~ '^[A-ZА-ЯІЇЄҐ][a-zа-яіїєґ]+$'), -- Перше слово з великої літери
+    Ідентифікатор VARCHAR(36) PRIMARY KEY,
+    Ім'я VARCHAR(50) NOT NULL CHECK (Ім'я SIMILAR TO '[A-ZА-ЯІЇЄҐ][a-zа-яіїєґ]+'),
+    Прізвище VARCHAR(50) NOT NULL CHECK (Прізвище SIMILAR TO '[A-ZА-ЯІЇЄҐ][a-zа-яіїєґ]+'),
     Дата_народження DATE NOT NULL
 );
 
 -- Створення таблиці "Фізичні показники"
 CREATE TABLE Фізичні_показники (
-    Ідентифікатор VARCHAR(36) PRIMARY KEY, -- UUID
-    Вага DOUBLE PRECISION NOT NULL CHECK (Вага > 0), -- Позитивна вага
-    Зріст DOUBLE PRECISION NOT NULL CHECK (Зріст > 0), -- Позитивний зріст
-    Об'єм_талії DOUBLE PRECISION CHECK (Об'єм_талії > 0), -- Позитивний об'єм
-    Рівень_жиру_в_організмі DOUBLE PRECISION CHECK (Рівень_жиру_в_організмі >= 0 AND Рівень_жиру_в_організмі <= 100), -- Відсоткове значення
+    Ідентифікатор VARCHAR(36) PRIMARY KEY,
+    Вага DOUBLE PRECISION NOT NULL CHECK (Вага > 0),
+    Зріст DOUBLE PRECISION NOT NULL CHECK (Зріст > 0),
+    Об'єм_талії DOUBLE PRECISION CHECK (Об'єм_талії > 0),
+    Рівень_жиру_в_організмі DOUBLE PRECISION CHECK (Рівень_жиру_в_організмі BETWEEN 0 AND 100),
     Дата_вимірювання DATE NOT NULL,
     Користувач_ID VARCHAR(36) NOT NULL,
     FOREIGN KEY (Користувач_ID) REFERENCES Користувач(Ідентифікатор)
@@ -20,12 +20,12 @@ CREATE TABLE Фізичні_показники (
 
 -- Створення таблиці "Система"
 CREATE TABLE Система (
-    Ідентифікатор VARCHAR(36) PRIMARY KEY -- UUID
+    Ідентифікатор VARCHAR(36) PRIMARY KEY
 );
 
 -- Створення таблиці "Аналіз"
 CREATE TABLE Аналіз (
-    Ідентифікатор VARCHAR(36) PRIMARY KEY, -- UUID
+    Ідентифікатор VARCHAR(36) PRIMARY KEY,
     Поточний_стан_здоров'я TEXT NOT NULL,
     Прогрес_користувача TEXT NOT NULL,
     Тренди_даних TEXT NOT NULL,
