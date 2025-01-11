4b48097570fadb71c1d9316043961aa0579f4a67
Лісєєв Кирило Михайлович <127152277+LiseevKirill@users.noreply.github.com>
2025-01-11 11:31:58 +0200

    Update DataSchema.sql

--- DataSchema.sql
+++ DataSchema.sql
@@ -1,34 +1,34 @@
--- Створення таблиці "Користувач"
-CREATE TABLE Користувач (
-    Ідентифікатор VARCHAR(36) PRIMARY KEY,
-    Ім'я VARCHAR(50) NOT NULL CHECK (Ім'я SIMILAR TO '[A-ZА-ЯІЇЄҐ][a-zа-яіїєґ]+'),
-    Прізвище VARCHAR(50) NOT NULL CHECK (Прізвище SIMILAR TO '[A-ZА-ЯІЇЄҐ][a-zа-яіїєґ]+'),
-    Дата_народження DATE NOT NULL
+-- Створення таблиці "User"
+CREATE TABLE User (
+    ID VARCHAR(36) PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL CHECK (FirstName SIMILAR TO '[A-Z][a-z]+'),
+    LastName VARCHAR(50) NOT NULL CHECK (LastName SIMILAR TO '[A-Z][a-z]+'),
+    DateOfBirth DATE NOT NULL
 );
 
--- Створення таблиці "Фізичні показники"
-CREATE TABLE Фізичні_показники (
-    Ідентифікатор VARCHAR(36) PRIMARY KEY,
-    Вага DOUBLE PRECISION NOT NULL CHECK (Вага > 0),
-    Зріст DOUBLE PRECISION NOT NULL CHECK (Зріст > 0),
-    Об'єм_талії DOUBLE PRECISION CHECK (Об'єм_талії > 0),
-    Рівень_жиру_в_організмі DOUBLE PRECISION CHECK (Рівень_жиру_в_організмі BETWEEN 0 AND 100),
-    Дата_вимірювання DATE NOT NULL,
-    Користувач_ID VARCHAR(36) NOT NULL,
-    FOREIGN KEY (Користувач_ID) REFERENCES Користувач(Ідентифікатор)
+-- Створення таблиці "PhysicalMetrics"
+CREATE TABLE PhysicalMetrics (
+    ID VARCHAR(36) PRIMARY KEY,
+    Weight DOUBLE PRECISION NOT NULL CHECK (Weight > 0),
+    Height DOUBLE PRECISION NOT NULL CHECK (Height > 0),
+    WaistCircumference DOUBLE PRECISION CHECK (WaistCircumference > 0),
+    BodyFatPercentage DOUBLE PRECISION CHECK (BodyFatPercentage BETWEEN 0 AND 100),
+    MeasurementDate DATE NOT NULL,
+    UserID VARCHAR(36) NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES User(ID)
 );
 
--- Створення таблиці "Система"
-CREATE TABLE Система (
-    Ідентифікатор VARCHAR(36) PRIMARY KEY
+-- Створення таблиці "System"
+CREATE TABLE System (
+    ID VARCHAR(36) PRIMARY KEY
 );
 
--- Створення таблиці "Аналіз"
-CREATE TABLE Аналіз (
-    Ідентифікатор VARCHAR(36) PRIMARY KEY,
-    Поточний_стан_здоров'я TEXT NOT NULL,
-    Прогрес_користувача TEXT NOT NULL,
-    Тренди_даних TEXT NOT NULL,
-    Фізичні_показники_ID VARCHAR(36) NOT NULL,
-    FOREIGN KEY (Фізичні_показники_ID) REFERENCES Фізичні_показники(Ідентифікатор)
+-- Створення таблиці "Analysis"
+CREATE TABLE Analysis (
+    ID VARCHAR(36) PRIMARY KEY,
+    HealthStatus TEXT NOT NULL,
+    UserProgress TEXT NOT NULL,
+    DataTrends TEXT NOT NULL,
+    PhysicalMetricsID VARCHAR(36) NOT NULL,
+    FOREIGN KEY (PhysicalMetricsID) REFERENCES PhysicalMetrics(ID)
 );
